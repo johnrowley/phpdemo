631f4ff511e187c7bb71ebf808944d4f8d553a76
training <training@STUDENT.IBAT>
2018-05-02 18:50:56 +0100

    Added loop

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
+<h1>This is a heading</h1>
+<?php echo "Hello World"; ?>
 <?php
 
-echo "Hello World"
+$listOfColours = ["red","green","blue"];
 
-?>
+for($i = 0; $i < $listOfColours.length; i++) {
+
+echo $listOfColours[$i];
+
+}
+
